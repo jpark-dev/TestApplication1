3f4719319cec85b61068b807d7f325eb2f0b4356
zeipar <zeipar@gmail.com>
2017-08-24 18:26:14 +0900

    Initial commit

---
+++ app/src/main/java/com/zeipar/testapplication/MyClass.java
@@ -0,0 +1,14 @@
+package com.zeipar.testapplication;
+
+/**
+ * Created by jsPark on 2017-08-24.
+ */
+
+public class MyClass {
+    public String text;
+    public MyClass(){
+        text = "HAHAHAHAHAHAHA";
+    }
+
+
+}
